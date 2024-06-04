e86bb0663d9431d937c990671374cd5776ad5fce
muhammadrayhanwidayat <murayat99@gmail.com>
2024-06-04 10:26:14 +0700

    menambahkan komen

--- StreamReaderandWriter_0152/StreamReaderandWriter_0152.cpp
+++ StreamReaderandWriter_0152/StreamReaderandWriter_0152.cpp
@@ -6,31 +6,52 @@ using namespace std;
 int main() {
 	string baris;
 
+
+	// membuka file dalam mode menulis
 	ofstream outfile;
+	//menunjuk ke sebuah nama file
 	outfile.open("contohfile.txt");
 
 	cout << ">= menulis file, \'q\' untuk keluar " << endl;
 
+
+	//unlimited loop untuk menulis
 	while (true) {
 		cout << "_ ";
+
+		//mendapatkan setiap karakter dalam satu baris
 		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
 		if (baris == "q") break;
+		//menulis dan memasukan nilai dari 'baris' ke dalam file 
 		outfile << baris << endl;
 
 	}
+
+	//selesai dalam menulis sekarang tutup filenya
 	outfile.close();
 
+
+	//membuka file dalam mode membaca
 	ifstream infile;
+
+	//menunjuk ke sebuah file
 	infile.open("contohfile.txt");
 
 	cout << endl << ">= Membuka dan membaca file " << endl;
 
+
+	//jika file ada maka
 	if (infile.is_open()) {
+		//melakukan perulaangan setiap baris
 		while (getline(infile, baris)) {
+			//dan tampilkan disini
 			cout << baris << '\n';
 		}
+		//tutup file tersebut setelah selesai
 		infile.close();
 	}
+	//jika tidak ditemuukan file maka akan menampilkan ini
 	else cout << "unable to open file ";
 	return 0;
 }
