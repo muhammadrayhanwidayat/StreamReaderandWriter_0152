305acd2265f933b30ec4334e0dd0edc8d653f6a4
muhammadrayhanwidayat <murayat99@gmail.com>
2024-06-04 10:34:19 +0700

    membuka file dan penunjuk ke file

--- iofilewithpath/iofilewithpath.cpp
+++ iofilewithpath/iofilewithpath.cpp
@@ -13,6 +13,11 @@ int main()
 
     cout << "Masukan Nama File : ";
     cin >> NamaFile;
+
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
    
 }
 
