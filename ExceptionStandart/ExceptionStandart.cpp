eea783cf8536c02e9317781b8eb1752b09ed7fb7
muhammadrayhanwidayat <murayat99@gmail.com>
2024-06-04 11:04:32 +0700

    membuat try cach dalam main

--- ExceptionStandart/ExceptionStandart.cpp
+++ ExceptionStandart/ExceptionStandart.cpp
@@ -7,6 +7,17 @@ using namespace std;
 
 int main()
 {
-    
+    cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = { 8, 4, 2 };
+        //pesan array integer 3 elemen
+        cout << data.at(5) << endl;
+
+    }
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+    }
 }
 
