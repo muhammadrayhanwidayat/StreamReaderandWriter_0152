364d9c81fbaf105e7a318c19c5dd8c19d6c5f44d
muhammadrayhanwidayat <murayat99@gmail.com>
2024-06-04 11:00:43 +0700

    membuat operasi try dan catch

--- ManualException/ManualException.cpp
+++ ManualException/ManualException.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 int main()
 {
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        //throw 0.5 //melakukan sebuah intereg maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        //blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+    catch (...) {
+        /*jika selain integer maka block ini akan dieksekusi*/
+        cout << "default pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
  
 }
 
